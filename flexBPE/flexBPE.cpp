b37414d2162e5f6288c9c9caf93ca549b630d068
David Pollack <david@da3.net>
2020-01-12 00:27:56 +0100

    remove clearing of merges vector

--- flexBPE/flexBPE.cpp
+++ flexBPE/flexBPE.cpp
@@ -298,8 +298,6 @@ void BPETrainer::learncodes(const uint32_t kNPairs, const char *inputFile1,
     count_in_word(words[wi], wi, counts[wi], pair_counts, contiguous_counts,
                   where_to_update);
   }
-  merges.clear();
-  merges.shrink_to_fit();
   find_maxp(contiguous_counts, max_p, max_c);
   for (size_t i = 0; i < kNPairs; i++) {
     // stop if no more merges can be made
